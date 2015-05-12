6ce5e9d4b4169c4756b500aea2371ee494182746
Daley Meistrell <dstrell14@gmail.com>
2015-05-12 13:16:39 -0700

    javascript win alert working for Tic Tac Toe

--- TicTacToe.js
+++ TicTacToe.js
@@ -8,12 +8,12 @@
    8) when one of the players reaches 5 points, notify which player won
           */
 var count = 0;
-var move=" ";
 var imageSrc1 = "images/player1.png"; 
 var imageSrc2 = "images/player2.png";
 
 var boxes = document.getElementsByClassName("box");
-		console.log(boxes)
+
+console.log(boxes)
 
 
 for (var i=0; i < boxes.length; i++){
@@ -21,25 +21,21 @@ for (var i=0; i < boxes.length; i++){
 }
 
 
-function takeTurn() {
-	count++;
-   if (count % 2 == 0) {
-		move = "O";
-} else {
-		move = "X";
-  }
+function takeTurn(i) {
+	if (i % 2 == 0) {
+		return "O";
+	} else {
+		return "X";
+	}
 }
-
-
 
 
 function addToOutput(click){
-	takeTurn();
-	this.innerHTML = move;
-	document.getElementsByClassName("box").innerHTML = move;
+	count++;
+	console.log(click)
+	this.innerHTML = takeTurn(count);
+	getWinner();
 }
-
-
 
 
 /*for (var i=0; i < boxes.length; i++){
@@ -48,24 +44,66 @@ function addToOutput(click){
 	}*/
 
 
+function getWinner(){
+	console.log(document.getElementById("spot1").innerText)
+	if(((document.getElementById("spot1").innerText == "X") &&
+	(document.getElementById("spot2").innerText == "X") &&
+	(document.getElementById("spot3").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
 
-// document.getElementById("solve").addEventListener("click",getSolution);
-// document.getElementById("clear").addEventListener("click",clear);
-// function getSolution(){
-// 	console.log('running get solution');
-// 	document.getElementById("console-area").innerHTML = eval(document.getElementById("console-area").innerHTML);
-// }
-// function clear(){
-// 	document.getElementById("console-area").innerHTML = null;
-// }
+	if(((document.getElementById("spot4").innerText == "X") &&
+	(document.getElementById("spot5").innerText == "X") &&
+	(document.getElementById("spot6").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
 
+	if(((document.getElementById("spot7").innerText == "X") &&
+	(document.getElementById("spot8").innerText == "X") &&
+	(document.getElementById("spot9").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
+	if(((document.getElementById("spot1").innerText == "X") &&
+	(document.getElementById("spot4").innerText == "X") &&
+	(document.getElementById("spot7").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
+	if(((document.getElementById("spot2").innerText == "X") &&
+	(document.getElementById("spot5").innerText == "X") &&
+	(document.getElementById("spot8").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
+	if(((document.getElementById("spot3").innerText == "X") &&
+	(document.getElementById("spot6").innerText == "X") &&
+	(document.getElementById("spot9").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
+	if(((document.getElementById("spot1").innerText == "X") &&
+	(document.getElementById("spot5").innerText == "X") &&
+	(document.getElementById("spot9").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
+	if(((document.getElementById("spot3").innerText == "X") &&
+	(document.getElementById("spot5").innerText == "X") &&
+	(document.getElementById("spot7").innerText == "X"))) {
+		alert("Player 1 Wins");
+	}
 
+}
+	/*if (spot1.innerText == "X" &&
+   spot2.innerText == "X" &&
+   spot3.innerText == "X")*/
 
-// playerWins [spot1, spot2, spot3]
-// 			  [spot4, spot5, spot6]
-// 			  [spot7, spot8, spot9]
-// 		      [spot1, spot4, spot7]
-// 			  [spot2, spot5, spot8]
-// 			  [spot3, spot6, spot9]
-// 			  [spot1, spot5, spot9]
-// 			  [spot3, spot5, spot7]
+/*document.getElementById*/
+var index;
+var playerWins =  ["spot1", "spot2", "spot3"];
+			      ["spot4", "spot5", "spot6"];
+			      ["spot7", "spot8", "spot9"];
+		          ["spot1", "spot4", "spot7"];
+			      ["spot2", "spot5", "spot8"];
+			      ["spot3", "spot6", "spot9"];
+			      ["spot1", "spot5", "spot9"];
+			      ["spot3", "spot5", "spot7"]
+for (index = 0; index < playerWins.length; index++) {
+
+}
