284c3a798acf981c9d0b335fd82079b2c86c0755
Daley Meistrell <dstrell14@gmail.com>
2015-05-13 16:35:35 -0700

    js incrementing, no 5 point winner

--- TicTacToe.js
+++ TicTacToe.js
@@ -4,9 +4,11 @@
    4) when one of the players reaches 5 points, notify which player won
           */
 var count = 0;
+var scoreLeft = 0;
+var scoreRight = 0;
 var boxes = document.getElementsByClassName("box");
 var gameBoard = ['', '', '', '', '', '', '', '', '']
-/*console.log(boxes)*/
+
 
 for (var i=0; i < boxes.length; i++){
 	boxes[i].addEventListener("click", addToOutput);
@@ -22,7 +24,6 @@ function takeTurn(i) {
 
 function addToOutput(click){
 	count++;
-	/*this.innerHTML = takeTurn(count);*/
 	if((takeTurn(count) == "2")) {
 		this.innerHTML = "<img class='full-opacity' id='firstStone' src='images/player2.png'>"
 	} else {
@@ -33,50 +34,126 @@ function addToOutput(click){
 }
 
 function getWinner(){
+	//console.log("first one")
+	console.log(gameBoard)
 	
 	if((gameBoard[0] == gameBoard[1]) && 
 	(gameBoard[1] == gameBoard[2]) && 
 	(gameBoard[0] != "")) {
-
-		console.log("first one");
-		console.log(gameBoard)
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[0] + " Wins")
+		if (gameBoard[0] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft);
+		} else if(gameBoard[0] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+
+		}
 	}
 	if((gameBoard[3] == gameBoard[4]) && 
 	(gameBoard[4] == gameBoard[5]) && 
 	(gameBoard[3] != "")) {
-		document.getElementById("winner").innerHTML = ("Player" + gameBoard[3] + " Wins")
+		document.getElementById("winner").innerHTML = ("Player " + gameBoard[3] + " Wins")
+	if (gameBoard[3] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[3] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[6] == gameBoard[7]) && 
 	(gameBoard[7] == gameBoard[8]) && 
 	(gameBoard[6] != "")) {
-		document.getElementById("winner").innerHTML = ("Player" + gameBoard[6] + " Wins")
+		document.getElementById("winner").innerHTML = ("Player " + gameBoard[6] + " Wins")
+	if (gameBoard[6] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[6] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[0] == gameBoard[3]) && 
 	(gameBoard[3] == gameBoard[6]) && 
 	(gameBoard[0] != "")) {
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[0] + " Wins")
+	if (gameBoard[0] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[0] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[1] == gameBoard[4]) && 
 	(gameBoard[4] == gameBoard[7]) && 
 	(gameBoard[1] != "")) {
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[1] + " Wins")
+	if (gameBoard[1] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[1] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[2] == gameBoard[5]) && 
 	(gameBoard[5] == gameBoard[8]) && 
 	(gameBoard[2] != "")) {
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[2] + " Wins")
+	if (gameBoard[2] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[2] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[0] == gameBoard[4]) && 
 	(gameBoard[4] == gameBoard[8]) && 
 	(gameBoard[0] != "")) {
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[0] + " Wins")
+	if (gameBoard[0] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[0] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
 	if((gameBoard[2] == gameBoard[4]) && 
 	(gameBoard[4] == gameBoard[6]) && 
 	(gameBoard[2] != "")) {
 		document.getElementById("winner").innerHTML = ("Player " + gameBoard[2] + " Wins")
+	if (gameBoard[2] == "1"){
+			document.getElementById("score1").innerHTML = (++scoreLeft)
+		} else if(gameBoard[2] = "2"){
+			document.getElementById("score2").innerHTML = (++scoreRight)
+		}
 	}
+	
+}
+function gameOver(){
+	if (scoreLeft >= "2"){
+		document.getElementById("winner").innerHTML = ("Game Over. Player 1 Wins")
+	} else if (scoreRight >= "2"){
+	 	document.getElementById("winner").innerHTML = ("Game Over. Player 2 Wins")
+	 }
+}
+
+
+function clearBoard(){
+	gameBoard = ['', '', '', '', '', '', '', '', '']
+	document.getElementById("spot1").innerHTML = "";
+	document.getElementById("spot2").innerHTML = "";
+	document.getElementById("spot3").innerHTML = "";
+	document.getElementById("spot4").innerHTML = "";
+	document.getElementById("spot5").innerHTML = "";
+	document.getElementById("spot6").innerHTML = "";
+	document.getElementById("spot7").innerHTML = "";
+	document.getElementById("spot8").innerHTML = "";
+	document.getElementById("spot9").innerHTML = "";
+	document.getElementById("winner").innerHTML = "Let's Play Again!";
+
+}
+function playGameTo(){
+	var scoreLeft;
+	var scoreRight;
+	if((scoreLeft >= 5) || (scoreRight >= 5)){
+		document.getElementById("winner").innerHTML = ('Game is Over! You Win!')
+	}
+}
+
+// function getScore(){ 
+// 	return score; 
+// } 
 
 	/*if(((document.getElementById("spot1").innerText == "X") &&
 	(document.getElementById("spot2").innerText == "X") &&
@@ -164,26 +241,4 @@ if(((document.getElementById("spot1").innerText == "O") &&
 	(document.getElementById("spot7").innerText == "O"))) {
 		document.getElementById("winner").innerHTML =("Player 2 Wins");
 	}	*/
-
-}
-
-function gainedPoint(){ 
-	score=0; 
-	score++; 
-} 
-function getScore(){ 
-	return score; 
-} 
 	
-// var index;
-// var playerWins =  ["spot1", "spot2", "spot3"]
-// 			      ["spot4", "spot5", "spot6"];
-// 			      ["spot7", "spot8", "spot9"];
-// 		          ["spot1", "spot4", "spot7"];
-// 			      ["spot2", "spot5", "spot8"];
-// 			      ["spot3", "spot6", "spot9"];
-// 			      ["spot1", "spot5", "spot9"];
-// 			      ["spot3", "spot5", "spot7"]
-// for (index = 0; index < playerWins.length; index++) {
-
-// }
